b8973a0ec3ddc430f5d3e0b8656030ef238819f2
Joe Chasinga <jo.chasinga@gmail.com>
2016-05-27 19:00:12 -0400

    First commit

---
+++ imports/ui/body.js
@@ -0,0 +1,56 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
+import './body.html';
+
+// This block execute when Template is loaded.
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
+// This block is all helpers of Template named body
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find(
+        { checked: {$ne: true } }, { sort: { createdAt: -1 }}
+      );
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 }});
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+});
+
+// This block is all events attached to body
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the Collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+})
